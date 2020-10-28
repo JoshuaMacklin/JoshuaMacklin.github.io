b3bf6cc4866b1030f871d70d9b2e5d36d90324a3
Joshua Macklin <69019114+JoshuaMacklin@users.noreply.github.com>
2020-10-28 00:41:15 -0400

    Added Dark Mode Toggle
    Dark mode for my entire website with a toggle at the top, that connects to a cookie to ensure that it keeps your option even after page reload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
- AOS.init({
+  AOS.init({
  	duration: 800,
  	easing: 'slide',
  	once: true
@@ -10,9 +10,9 @@ jQuery(document).ready(function($) {
 
 	// loader
 	$(".loader").delay(1000).fadeOut("slow");
-  $("#overlayer").delay(1000).fadeOut("slow");	
+  $("#overlayer").delay(1000).fadeOut("slow");
 
-	
+
 	var siteMenuClone = function() {
 
 		$('.js-clone-nav').each(function() {
@@ -22,11 +22,11 @@ jQuery(document).ready(function($) {
 
 
 		setTimeout(function() {
-			
+
 			var counter = 0;
       $('.site-mobile-menu .has-children').each(function(){
         var $this = $(this);
-        
+
         $this.prepend('<span class="arrow-collapse collapsed">');
 
         $this.find('.arrow-collapse').attr({
@@ -52,8 +52,8 @@ jQuery(document).ready(function($) {
       } else {
         $this.addClass('active');
       }
-      e.preventDefault();  
-      
+      e.preventDefault();
+
     });
 
 		$(window).resize(function() {
@@ -78,7 +78,7 @@ jQuery(document).ready(function($) {
 				$('body').addClass('offcanvas-menu');
 				$this.addClass('active');
 			}
-		}) 
+		})
 
 		// click outisde offcanvas
 		$(document).mouseup(function(e) {
@@ -89,7 +89,7 @@ jQuery(document).ready(function($) {
 				}
 	    }
 		});
-	}; 
+	};
 	siteMenuClone();
 
 
@@ -254,7 +254,7 @@ jQuery(document).ready(function($) {
 			    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
 			});
 		}
-				
+
 	};
 	siteCountDown();
 
@@ -289,7 +289,7 @@ jQuery(document).ready(function($) {
         setTimeout(function() {
         	$('body').removeClass('offcanvas-menu');
         }, 20);
-        
+
       });
 
     });
@@ -312,7 +312,7 @@ jQuery(document).ready(function($) {
   			$('body').removeClass('offcanvas-menu');
   		}
 
-  	}) 
+  	})
 
   };
   siteScroll();
@@ -329,7 +329,7 @@ jQuery(document).ready(function($) {
 	      columnWidth: '.col-sm-3'
 	    });
 	  });
-	  
+
 	  $container.isotope({ filter: '*' });
 
 	    // filter items on button click
